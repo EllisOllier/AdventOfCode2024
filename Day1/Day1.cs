a1df70145395c6f968d7d02e41b8a08bda618880
EllisOllier <ellisollier1@gmail.com>
2024-12-01 12:28:47 +0000

    Add project files.

---
+++ Day1/Day1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day1{
+    internal class Day1{
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
