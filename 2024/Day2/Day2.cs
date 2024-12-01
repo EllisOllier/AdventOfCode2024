0f6d53faf6c0d44b576f82717eb27c6557e7df22
EllisOllier <ellisollier1@gmail.com>
2024-12-01 14:11:59 +0000

    Added Day2

---
+++ 2024/Day2/Day2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2024.Day2
+{
+    internal class Day2
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
