12383a5bf3cdf83314cb06aaad7a66ac6ff14b07
EllisOllier <ellisollier1@gmail.com>
2024-12-01 14:05:17 +0000

    Added Part two

--- 2024/Day1/Day1.cs
+++ 2024/Day1/Day1.cs
@@ -9,17 +9,21 @@ using System.Collections;
 namespace AdventOfCode.Day1{
     internal class Day1{
         static void Main(string[] args){
-            int total = 0;
-            // Init reader
+            int distanceTotal = 0;
+
             StreamReader reader = new StreamReader("../../../2024/Day1/Input.txt");
-            int readerLength = File.ReadAllLines("../../../2024/Day1/Input.txt").Length;
             string currentLine;
+
             ArrayList leftList = new ArrayList();
             ArrayList rightList = new ArrayList();
+            int[] leftArray;
+            int[] rightArray;
+
+            // Part one
             while ((currentLine = reader.ReadLine()) != null)
             {
-                string[] splitString = currentLine.Split(" ");
                 // Spit into two halves
+                string[] splitString = currentLine.Split(" ");
                 // Index 0 first num, index 3 second num
                 int firstNumber = Int32.Parse(splitString[0]);
                 int secondNumber = Int32.Parse(splitString[3]);
@@ -27,21 +31,37 @@ namespace AdventOfCode.Day1{
                 leftList.Add(firstNumber);
                 rightList.Add(secondNumber);
             }
+
             // sort arraylist
             leftList.Sort();
             rightList.Sort();
-            // Convert to array
-            int[] leftArray = leftList.ToArray(typeof(int)) as int[];
-            int[] rightArray = rightList.ToArray(typeof(int)) as int[];
+            // Convert arrayList to array
+            leftArray = leftList.ToArray(typeof(int)) as int[];
+            rightArray = rightList.ToArray(typeof(int)) as int[];
             // Check distance
             for (int i = 0; i < leftArray.Length; i++)
             {
-                Console.WriteLine(leftArray[i] + " " + rightArray[i]);
+                //Console.WriteLine(leftArray[i] + " " + rightArray[i]);
                 // add distance to total
-                total += Math.Abs(leftArray[i] - rightArray[i]);
+                distanceTotal += Math.Abs(leftArray[i] - rightArray[i]);
             }
-            Console.WriteLine("Final total: " + total);
-            
+            Console.WriteLine("Distance total: " + distanceTotal);
+
+            // Part two
+            int xOfSameNumber = 0;
+            int similarityScore = 0;
+            for (int i = 0; i < leftArray.Length - 1; i++)
+            {
+                xOfSameNumber = 0;
+                for (int j = 0; j < rightArray.Length; j++)
+                {
+                    if (rightArray[i] == leftArray[j]) {
+                        xOfSameNumber++;
+                    }
+                }
+                similarityScore += rightArray[i] * xOfSameNumber;
+            }
+            Console.WriteLine("Similarity Score: " + similarityScore);
         }
     }
 }
